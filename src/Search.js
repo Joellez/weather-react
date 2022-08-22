0b46be957ba31ee409d0326d7c6f2ff4ef8bd16b
Joelle Zane <Joellezane@hotmail.com>
2022-08-22 15:59:15 +0100

    centred icon

--- src/Search.js
+++ src/Search.js
@@ -8,14 +8,17 @@ export default function Search() {
       <div className="row pb-4">
         <div className="col-md-6">
           <form id="my-form">
-            <div className="input-group mb-3 city-search " id="city-search-form">
+            <div
+              className="input-group mb-3 city-search "
+              id="city-search-form"
+            >
               <input
                 type="text"
                 className="form-control rounded"
                 placeholder="Enter a city"
                 aria-label="City Search"
                 aria-describedby="basic-addon2"
-                autocomplete="off"
+                autoComplete="off"
                 id="search-bar"
               />
               <div className="input-group-append" id="form-input">
@@ -27,7 +30,11 @@ export default function Search() {
           </form>
         </div>
         <div className="col-md-6">
-          <button type="submit" className="btn btn-light location-button" id="location-button">
+          <button
+            type="submit"
+            className="btn btn-light location-button"
+            id="location-button"
+          >
             Use current location
           </button>
         </div>
